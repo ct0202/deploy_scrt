9cb1ce67fd67707bbcc8607f427b4bdcd673527d
ct0202 <arman.jumashev.dev@gmail.com>
2025-02-27 12:22:56 +0500

    3

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import Chat from "./pages/Chat"
 import StreamFilters from "./pages/StreamFilters";
 import Match from "./pages/Match";
 
+import ScrollBlocker from "./ScrollBlocker";
+
 function App() {
   useEffect(() => {
     const tg = window.Telegram.WebApp;
@@ -27,6 +29,7 @@ function App() {
         className="App flex flex-col justify-start items-center w-[100%] h-[100%] [background:linear-gradient(180deg,rgb(1,13,13)_0%,rgb(3.01,42.5,42.5)_100%)] bg-[100%_100%]"
         style={{ height: "100vh" }}
       >
+        <ScrollBlocker />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" index element={<HomePage />} />
---
+++ src/ScrollBlocker.js
@@ -0,0 +1,19 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+const ScrollBlocker = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        const blockedRoutes = ["/"]; // Укажи URL страниц без скролла
+        if (blockedRoutes.includes(location.pathname)) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "";
+        }
+    }, [location.pathname]);
+
+    return null; // Компонент ничего не рендерит, просто управляет стилями
+};
+
+export default ScrollBlocker;
