01a0345007773e96e58486ec9a5f053fe7831b81
ct0202 <arman.jumashev.dev@gmail.com>
2025-03-21 21:06:16 +0500

    123

--- src/App.jsx
+++ src/App.jsx
@@ -41,13 +41,13 @@ function App() {
     // tg.requestFullscreen();
     // window.Telegram.WebApp.ready();
     // window.Telegram.WebApp.expand();
-    if (0) {
+    if (1) {
       const tg = window.Telegram.WebApp;
       tg.disableVerticalSwipes();
       tg.requestFullscreen();
       tg.ready();
       tg.expand();
-      alert('19');
+      alert('20');
     }
   }, []);
 
