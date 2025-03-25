abd5aa4a5bdc720d1d9abfe8c835a1f065059192
ct0202 <arman.jumashev.dev@gmail.com>
2025-03-26 01:17:32 +0500

    123

--- src/pages/FullChat.jsx
+++ src/pages/FullChat.jsx
@@ -41,7 +41,7 @@ function FullChat () {
 
 
     return (
-        <div className='w-[100vw] pt-[90px] flex justify-center items-center flex-col'>
+        <div className='w-[100vw] pt-[90px] flex justify-center items-center flex-col relative'>
             <div className='flex items-center justify-center text-white text-[24px] w-full pb-[26px] pt-[26px]
             border-b border-[#233636]'>
                 <div className='w-[100vw] flex items-center justify-center'>
