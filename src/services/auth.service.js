6d5e25a93732134b2ff7c0e36d7f957a395e3cab
ct0202 <arman.jumashev.dev@gmail.com>
2025-04-27 17:43:03 +0500

    test

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -22,6 +22,7 @@ export const useAuth = () => {
         {
             try {
                 if (isTelegram) {
+                    console.log('isTelegram');
                     const tg = window.Telegram.WebApp;
                     tg.disableVerticalSwipes();
                     tg.requestFullscreen();
